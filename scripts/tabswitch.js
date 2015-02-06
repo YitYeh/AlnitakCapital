7e1fd8fb038ded4b8a255ad658ab2a8333093ce2
Jason Katz <jkatz94@gmail.com>
2015-02-06 15:09:34 -0500

    Manipulated hashchange to change content on back

--- scripts/tabswitch.js
+++ scripts/tabswitch.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-	$('#tab-1').fadeIn();
+	location.hash = '#home';
+	$('#home').fadeIn();
     $(".menu-item").click(function(event) {
         event.preventDefault();
         $(this).addClass("current");
@@ -7,5 +8,13 @@ $(document).ready(function() {
         var tab = $(this).find('a').attr("href");
         $(".tab-content").not(tab).css("display", "none");
         $(tab).fadeIn();
+		
+		location.hash = $(this).find('a').attr('href');
     });
+});
+
+$(window).on("hashchange", function(e) {
+	// Display proper content
+	$(".tab-content").not(location.hash).css("display", "none");
+	$(location.hash).fadeIn();
 });
