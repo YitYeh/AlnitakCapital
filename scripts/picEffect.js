f170b63ed337e43c7efdb045b83ef564dd9494f4
Tom <tomkoch96@yahoo.com>
2015-02-17 13:22:56 -0500

    Added background pic effect

---
+++ scripts/picEffect.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+	$backPic = $('.backpic');
+	$logoPic = $('.logopic');
+	
+	$backPic.animate({
+		opacity:.2
+	},2000);
+	$logoPic.animate({
+		opacity:1
+	},2000);
+	
+});
